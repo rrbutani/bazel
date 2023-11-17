rrbutani/bazel

commit 491284b0c782e0df25dd0a8bc921a9c2de0e6881

Do not do magic to achieve a hermetic `/tmp` when the sandbox is hermetic.
The sandbox process already does a `chroot()` and creates a fresh `/tmp` in it so no magic is needed.

RELNOTES: None.
PiperOrigin-RevId: 579782553
Change-Id: Ia5df1911ab326b739a0693ae81c794ecd8dce53d

src/main/java/com/google/devtools/build/lib/sandbox/LinuxSandboxedSpawnRunner.java
@@ -187,6 +187,12 @@ final class LinuxSandboxedSpawnRunner extends AbstractSandboxSpawnRunner {
       return false;
     }
 
+    if (getSandboxOptions().useHermetic) {
+      // The hermetic sandbox is, well, already hermetic. Also, it creates an empty /tmp by default
+      // so nothing needs to be done to achieve a /tmp that is also hermetic.
+      return false;
+    }
+
     boolean tmpExplicitlyBindMounted =
         getSandboxOptions().sandboxAdditionalMounts.stream()
             .anyMatch(e -> e.getKey().equals("/tmp"));

commit 22a2e841a9fa69f09b30d32cd9b579f51667b260

sandbox: fix the fallout from `491284b0c782e0df25dd0a8bc921a9c2de0e6881`
also attempts (unsuccessfully; it's still broken) to unbreak hermetic
tmp when used without the hermetic sandbox

this matters more now because Bazel 7 has the hermetic tmp enabled by
default

src/main/java/com/google/devtools/build/lib/sandbox/LinuxSandboxedSpawnRunner.java
@@ -210,7 +210,7 @@ final class LinuxSandboxedSpawnRunner extends AbstractSandboxSpawnRunner {
     if (getSandboxOptions().useHermetic) {
       // The hermetic sandbox is, well, already hermetic. Also, it creates an empty /tmp by default
       // so nothing needs to be done to achieve a /tmp that is also hermetic.
-      return false;
+      return true; // !! TODO: this is a bit of a hack
     }
 
     boolean tmpExplicitlyBindMounted =
@@ -380,7 +380,16 @@ final class LinuxSandboxedSpawnRunner extends AbstractSandboxSpawnRunner {
         // expects host paths for the working directory!
         commandLineBuilder.setWorkingDirectory(sandboxExecRoot);
       } else {
-        commandLineBuilder.setWorkingDirectory(withinSandboxWorkingDirectory);
+        // TODO: is this necessary? (does it have to be different than ^)
+        // commandLineBuilder.setWorkingDirectory(sandboxTmp
+        //   // .getRelative(withinSandboxWorkingDirectory.asFragment())
+        //   .getRelative(BAZEL_WORKING_DIRECTORY)
+        //   .getRelative(workspaceName)
+        // );
+
+        commandLineBuilder.setWorkingDirectory(sandboxExecRoot);
+
+        // commandLineBuilder.setWorkingDirectory(withinSandboxWorkingDirectory);
       }
     }
 

src/main/java/com/google/devtools/build/lib/sandbox/SandboxOptions.java
@@ -355,7 +355,7 @@ public class SandboxOptions extends OptionsBase {
 
   @Option(
       name = "incompatible_sandbox_hermetic_tmp",
-      defaultValue = "true",
+      defaultValue = "false", // !!! fix
       documentationCategory = OptionDocumentationCategory.EXECUTION_STRATEGY,
       effectTags = {OptionEffectTag.EXECUTION},
       help =